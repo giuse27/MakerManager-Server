4ba4b313e0dfd1fb87a831e08ba98a298cd9a3bc
giuse27 <giuseppe.vaglica3@gmail.com>
2026-06-16 14:44:16 +0200

    Creazione base del server per MakerManager

---
+++ src/main/java/it/unipi/makermanagerserver/MakerManagerServerApplication.java
@@ -0,0 +1,13 @@
+package it.unipi.makermanagerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakerManagerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakerManagerServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unipi/makermanagerserver/MakerManagerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unipi.makermanagerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakerManagerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
