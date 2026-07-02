45e85501ddb1495e72b5e875e973d115b2d40773
giuse27 <giuseppe.vaglica3@gmail.com>
2026-07-02 18:16:10 +0200

    Repository JPA per ProgettoMaker

--- src/main/java/it/unipi/makermanagerserver/repository/ProgettoMakerRepository.java
+++ src/main/java/it/unipi/makermanagerserver/repository/ProgettoMakerRepository.java
@@ -1,5 +1,18 @@
 package it.unipi.makermanagerserver.repository;
 
-public class ProgettoMakerRepository {
+import it.unipi.makermanagerserver.model.project.ProgettoMaker;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ProgettoMakerRepository extends JpaRepository<ProgettoMaker, Long> {
     
+    // Cerca progetti per nome
+    List<ProgettoMaker> findByNomeContainingIgnoreCase(String nome);
+    
+    // JPA permette di navigare dentro gli oggetti @Embedded
+    // Questo metodo cerca i progetti in base allo stato contenuto dentro la classe Progresso
+    List<ProgettoMaker> findByProgressoStatoAvanzamento(String stato);
+
 }
