f2f5b2413f1e8932a7ff8516175d553baa326a13
giuse27 <giuseppe.vaglica3@gmail.com>
2026-07-02 22:40:34 +0200

    DTO per i progetti

--- src/main/java/it/unipi/makermanagerserver/dto/init/ProgettoInitDTO.java
+++ src/main/java/it/unipi/makermanagerserver/dto/init/ProgettoInitDTO.java
@@ -1,5 +1,7 @@
 package it.unipi.makermanagerserver.dto.init;
 
+import java.util.List;
+
 /**
  * DTO "di caricamento" per una voce dell'array "progetti" nel JSON di
  * inizializzazione. Vedi ElementoCatalogoInitDTO per la spiegazione
@@ -10,5 +12,46 @@ package it.unipi.makermanagerserver.dto.init;
  * decidere quale sottoclasse istanziare.
  */
 public class ProgettoInitDTO {
+
+    private String tipo;
+    private String nome;
+    private String descrizione;
+    private List<RigaBOMInitDTO> bom;
+
+    public ProgettoInitDTO() {
+
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public List<RigaBOMInitDTO> getBom() {
+        return bom;
+    }
+
+    public void setBom(List<RigaBOMInitDTO> bom) {
+        this.bom = bom;
+    }
     
 }
