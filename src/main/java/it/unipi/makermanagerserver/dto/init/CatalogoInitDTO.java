c58f9bfb34e5cb3d0619fd2e9d3b6e476baebcad
giuse27 <giuseppe.vaglica3@gmail.com>
2026-07-02 22:01:21 +0200

    Creazione del DTO per il catalogo-iniziale.json

---
+++ src/main/java/it/unipi/makermanagerserver/dto/init/CatalogoInitDTO.java
@@ -0,0 +1,18 @@
+package it.unipi.makermanagerserver.dto.init;
+
+/*
+ * DTO radice che rispecchia l'intera struttura del file
+ * catalogo-iniziale.json (le 4 sezioni: catalogo, inventari,
+ * articoliInventario, progetti).
+ *
+ * Jackson (gia' incluso in spring-boot-starter-webmvc) mappa
+ * automaticamente le proprieta' del JSON sui campi con lo stesso nome,
+ * senza bisogno di configurazione aggiuntiva: e' per questo che i nomi
+ * dei campi qui sotto devono corrispondere esattamente alle chiavi
+ * di primo livello nel JSON.
+ */
+public class CatalogoInitDTO {
+
+    
+    
+}
