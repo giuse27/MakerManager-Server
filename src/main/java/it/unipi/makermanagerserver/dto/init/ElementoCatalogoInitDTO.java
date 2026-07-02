a6c6fa52b1e261e2253c758f6841cbcf6dd100f9
giuse27 <giuseppe.vaglica3@gmail.com>
2026-07-02 22:38:19 +0200

    Implementazione del DTO per l'ElementoCatalogo

--- src/main/java/it/unipi/makermanagerserver/dto/init/ElementoCatalogoInitDTO.java
+++ src/main/java/it/unipi/makermanagerserver/dto/init/ElementoCatalogoInitDTO.java
@@ -13,5 +13,37 @@ package it.unipi.makermanagerserver.dto.init;
  * un valore non valido vogliamo poterlo intercettare noi con un errore chiaro
  */
 public class ElementoCatalogoInitDTO {
+
+    private String nome;
+    private String descrizione;
+    private String tipologia;
+
+    public ElementoCatalogoInitDTO() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescrizione() {
+        return descrizione;
+    }
+
+    public void setDescrizione(String descrizione) {
+        this.descrizione = descrizione;
+    }
+
+    public String getTipologia() {
+        return tipologia;
+    }
+
+    public void setTipologia(String tipologia) {
+        this.tipologia = tipologia;
+    }
     
 }
