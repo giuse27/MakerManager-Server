011d03d470aa6221c33ef827ac36bbb2245c9958
giuse27 <giuseppe.vaglica3@gmail.com>
2026-07-02 15:10:32 +0200

    Enum per definire la tipologia di un ElementoCatalogo

---
+++ src/main/java/it/unipi/makermanagerserver/enums/TipologiaElemento.java
@@ -0,0 +1,14 @@
+package it.unipi.makermanagerserver.enums;
+
+/**
+ * Categorie generali con cui un ElementoCatalogo può essere classificato
+ * @author giuse27
+ */
+public enum TipologiaElemento {
+    
+    COMPONENTE_ELETTRONICO,
+    MATERIALE_CONSUMABILE,
+    ATTREZZO_DA_LAVORO,
+    SOFTWARE
+
+}
