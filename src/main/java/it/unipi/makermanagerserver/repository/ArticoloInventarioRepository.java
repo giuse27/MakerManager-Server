4605373ed4133e9085fba0eac0dcf5bae857a0f9
giuse27 <giuseppe.vaglica3@gmail.com>
2026-07-02 18:14:39 +0200

    Repository JPA per ArticoloInventario

--- src/main/java/it/unipi/makermanagerserver/repository/ArticoloInventarioRepository.java
+++ src/main/java/it/unipi/makermanagerserver/repository/ArticoloInventarioRepository.java
@@ -1,5 +1,19 @@
 package it.unipi.makermanagerserver.repository;
 
-public class ArticoloInventarioRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import it.unipi.makermanagerserver.model.inventory.ArticoloInventario;
+
+@Repository
+public interface ArticoloInventarioRepository extends JpaRepository<ArticoloInventario, Long> {
+    
+    // Trova gli articoli che stanno per finire nel cassetto (Quantità minore o uguale a una soglia)
+    List<ArticoloInventario> findByQuantitaInPossessoLessThanEqual(int soglia);
+    
+    // Cerca un oggetto nell'inventario partendo dal suo descriptor teorico del catalogo
+    List<ArticoloInventario> findByElementoCatalogoId(Long elementoCatalogoId);
     
 }
