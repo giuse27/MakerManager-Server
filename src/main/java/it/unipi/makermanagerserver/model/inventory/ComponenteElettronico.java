fefe8eb7e568c1b04355ad4702bd5de3f605dd90
giuse27 <giuseppe.vaglica3@gmail.com>
2026-07-02 15:43:52 +0200

    Implementazione temporanea delle sottoclassi specializzate

--- src/main/java/it/unipi/makermanagerserver/model/inventory/ComponenteElettronico.java
+++ src/main/java/it/unipi/makermanagerserver/model/inventory/ComponenteElettronico.java
@@ -1,5 +1,24 @@
 package it.unipi.makermanagerserver.model.inventory;
 
-public class ComponenteElettronico {
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+/**
+ * Sottoclasse di ArticoloInventario per componenti elettronici
+ * (es. resistenze, microcontrollori, sensori).
+ * Campi tecnici specifici (voltaggio, corrente, ecc.) verranno rifiniti
+ * in seguito: per ora e' uno scheletro minimo, coerente con la fase v0.0.0.
+ */
+@Entity
+@DiscriminatorValue("COMPONENTE_ELETTRONICO")
+public class ComponenteElettronico extends ArticoloInventario {
+
+    public ComponenteElettronico() {
+        super();
+    }
+
+    public ComponenteElettronico(ElementoCatalogo elementoCatalogo, Inventario inventario, int quantita) {
+        super(elementoCatalogo, inventario, quantita);
+    }
     
 }
--- src/main/java/it/unipi/makermanagerserver/model/inventory/MaterialeConsumabile.java
+++ src/main/java/it/unipi/makermanagerserver/model/inventory/MaterialeConsumabile.java
@@ -1,5 +1,24 @@
 package it.unipi.makermanagerserver.model.inventory;
 
-public class MaterialeConsumabile {
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+/**
+ * Sottoclasse di ArticoloInventario per materiali consumabili
+ * (es. bobine di filamento PLA, resina, colla).
+ * Campi tecnici specifici (stato di usura, scadenza, ecc.) verranno rifiniti
+ * in seguito: per ora e' uno scheletro minimo, coerente con la fase v0.0.0.
+ */
+@Entity
+@DiscriminatorValue("MATERIALE_CONSUMABILE")
+public class MaterialeConsumabile extends ArticoloInventario {
+
+    public MaterialeConsumabile() {
+        super();
+    }
+
+    public MaterialeConsumabile(ElementoCatalogo elementoCatalogo, Inventario inventario, int quantita) {
+        super(elementoCatalogo, inventario, quantita);
+    }
     
 }
