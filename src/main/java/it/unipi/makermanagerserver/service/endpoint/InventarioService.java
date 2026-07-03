aa2ece5c235a7c924dbafa9db959ab5c4f0d1523
giuse27 <giuseppe.vaglica3@gmail.com>
2026-07-03 22:22:27 +0200

    Costruttore per il service

--- src/main/java/it/unipi/makermanagerserver/service/endpoint/InventarioService.java
+++ src/main/java/it/unipi/makermanagerserver/service/endpoint/InventarioService.java
@@ -1,5 +1,37 @@
 package it.unipi.makermanagerserver.service.endpoint;
 
+import org.springframework.stereotype.Service;
+
+import it.unipi.makermanagerserver.mapper.ArticoloInventarioMapper;
+import it.unipi.makermanagerserver.mapper.InventarioMapper;
+import it.unipi.makermanagerserver.repository.ArticoloInventarioRepository;
+import it.unipi.makermanagerserver.repository.InventarioRepository;
+
+/**
+ * Questa classe fornisce metodi per l'endpoint /api/inventario/*
+ * 
+ * Il controller usa questa classe come intermediaria con il repository o mapper
+ */
+@Service
 public class InventarioService {
-    
+
+    private final InventarioRepository inventarioRepo;
+    private final ArticoloInventarioRepository articoloRepo;
+    private final InventarioMapper inventarioMapper;
+    private final ArticoloInventarioMapper articoloMapper;
+
+    public InventarioService(
+        InventarioRepository inventarioRepo,
+        ArticoloInventarioRepository articoloRepo,
+        InventarioMapper inventarioMapper,
+        ArticoloInventarioMapper articoloMapper
+    ) {
+
+        this.inventarioRepo = inventarioRepo;
+        this.articoloRepo = articoloRepo;
+        this.inventarioMapper = inventarioMapper;
+        this.articoloMapper = articoloMapper;
+
+    }
+
 }
