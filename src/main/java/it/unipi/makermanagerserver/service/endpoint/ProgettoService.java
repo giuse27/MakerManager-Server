f470345bd87172dfc5e7da80cb2e469633581261
giuse27 <giuseppe.vaglica3@gmail.com>
2026-07-04 16:09:54 +0200

    aggiunta della repository e del mapper per i progetti

--- src/main/java/it/unipi/makermanagerserver/service/endpoint/ProgettoService.java
+++ src/main/java/it/unipi/makermanagerserver/service/endpoint/ProgettoService.java
@@ -6,10 +6,25 @@ import org.springframework.stereotype.Service;
 
 import it.unipi.makermanagerserver.dto.progetto.ProgettoRequestDTO;
 import it.unipi.makermanagerserver.dto.progetto.ProgettoResponseDTO;
+import it.unipi.makermanagerserver.mapper.ProgettoMapper;
+import it.unipi.makermanagerserver.repository.ProgettoMakerRepository;
 
 @Service
 public class ProgettoService {
 
+    private final ProgettoMakerRepository progettoRepo;
+    private final ProgettoMapper progettoMapper;
+
+    public ProgettoService(
+        ProgettoMakerRepository progettoRepo,
+        ProgettoMapper progettoMapper
+    ) {
+ 
+        this.progettoRepo = progettoRepo;
+        this.progettoMapper = progettoMapper;
+ 
+    }
+    
     public List<ProgettoResponseDTO> trovaTutti() {
 
         // TODO Auto-generated method stub
