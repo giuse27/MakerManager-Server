4518e315f7e3684a229eb1541f1655a776dcba34
giuse27 <giuseppe.vaglica3@gmail.com>
2026-07-06 16:19:22 +0200

    Repository per l'utente

---
+++ src/main/java/it/unipi/makermanagerserver/repository/UtenteRepository.java
@@ -0,0 +1,23 @@
+package it.unipi.makermanagerserver.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import it.unipi.makermanagerserver.model.user.Utente;
+
+@Repository
+public interface UtenteRepository extends JpaRepository<Utente, Long> {
+
+    // cerca un utente sulla base del suo nickname
+    Optional<Utente> findByNickname(String nickname);
+
+    // Cerca un utente per email: e' l'identificativo usato per il login
+    Optional<Utente> findByEmail(String email);
+
+    // Verifica in fase di registrazione se email o nickname sono gia' in uso
+    boolean existsByEmail(String email);
+    boolean existsByNickname(String nickname);
+
+}
