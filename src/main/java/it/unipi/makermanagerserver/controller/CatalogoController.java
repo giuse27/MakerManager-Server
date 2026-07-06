ec6b26c1f2c130e9e0cc1bef922541d57146bca1
giuse27 <giuseppe.vaglica3@gmail.com>
2026-07-06 23:48:24 +0200

    aggiunta la validazione sui dati

--- src/main/java/it/unipi/makermanagerserver/controller/CatalogoController.java
+++ src/main/java/it/unipi/makermanagerserver/controller/CatalogoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -53,7 +54,7 @@ public class CatalogoController {
      */
     @PostMapping
     public ResponseEntity<ElementoCatalogoResponseDTO> crea(
-        @RequestBody ElementoCatalogoRequestDTO dtoRichiesta
+        @Validated @RequestBody ElementoCatalogoRequestDTO dtoRichiesta
     ) {
 
         ElementoCatalogoResponseDTO dtoRisposta = catalogoService.crea(dtoRichiesta);
