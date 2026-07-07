c825865b3a111ea30c825ae2a59f0a1b15f24875
giuse27 <giuseppe.vaglica3@gmail.com>
2026-07-07 22:53:25 +0200

    creazione del service per la raccomandazione di progetti

---
+++ src/main/java/it/unipi/makermanagerserver/service/business/ProgettiConsigliatiService.java
@@ -0,0 +1,11 @@
+package it.unipi.makermanagerserver.service.business;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * ProgettiConsigliatiService
+ */
+@Service
+public class ProgettiConsigliatiService {
+
+}
