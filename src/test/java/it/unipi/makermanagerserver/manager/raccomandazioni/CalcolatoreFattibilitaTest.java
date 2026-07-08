eca589675c898e9dd1c67a98986a3a8e21089f6f
giuse27 <giuseppe.vaglica3@gmail.com>
2026-07-08 16:35:26 +0200

    test con quantità insufficiente o assente

--- src/test/java/it/unipi/makermanagerserver/manager/raccomandazioni/CalcolatoreFattibilitaTest.java
+++ src/test/java/it/unipi/makermanagerserver/manager/raccomandazioni/CalcolatoreFattibilitaTest.java
@@ -1,6 +1,7 @@
 package it.unipi.makermanagerserver.manager.raccomandazioni;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
@@ -22,6 +23,8 @@ public class CalcolatoreFattibilitaTest {
     // TEST
 
     /**
+     * TEST 1 con BOM1 e DISP1
+     * 
      * In questo test passiamo BOM1 e DISP1 e ci aspettiamo che il progetto sia
      * interamente realizzabile
      */
@@ -36,6 +39,49 @@ public class CalcolatoreFattibilitaTest {
         assertEquals(1.0, esito.indiceFattibilita());
         assertTrue(esito.dettaglioMancanti().isEmpty());
 
+    }
+
+    /**
+     * TEST 2 CON BOM1 e DISP2
+     * 
+     * In questo test andremo a verificare l'esito di un progetto con un elemento
+     * posseduto di cui non si ha sufficiente quantità
+     */
+    @Test
+    void testDettaglioElementiMancantiConQuantitaInsufficiente() {
+
+        EsitoFattibilita esito = calcolatore.calcola(BOM1, DISP2);
+
+        assertFalse(esito.realizzabile());
+        assertEquals(1, esito.righeMancanti());
+        assertEquals(5, esito.righeTotali());
+        assertEquals(0.8, esito.indiceFattibilita());
+        assertTrue(esito.dettaglioMancanti().size() == 1);
+        assertEquals("Pulsanti", esito.dettaglioMancanti().getFirst().nomeElemento());
+        assertEquals(1, esito.dettaglioMancanti().getFirst().quantitaMancante());
+
+    }
+
+    /**
+     * TEST 3 con BOM1 e DISP3
+     * 
+     * In questo test andremo a verificare l'esito di un progetto con un elemento
+     * richiesto di cui non si possiede nemmeno una quantità
+     */
+    @Test
+    void testDettaglioElementiMancantiConQuantitaNulla() {
+
+        EsitoFattibilita esito = calcolatore.calcola(BOM1, DISP3);
+
+        assertFalse(esito.realizzabile());
+        assertEquals(1, esito.righeMancanti());
+        assertEquals(5, esito.righeTotali());
+        assertEquals(0.8, esito.indiceFattibilita());
+        assertTrue(esito.dettaglioMancanti().size() == 1);
+        assertEquals("Led", esito.dettaglioMancanti().getFirst().nomeElemento());
+        assertEquals(1, esito.dettaglioMancanti().getFirst().quantitaMancante());
+        assertEquals(0, esito.dettaglioMancanti().getFirst().quantitaPosseduta());
+        
     }
 
     // UTILITY
@@ -84,6 +130,27 @@ public class CalcolatoreFattibilitaTest {
         2L, 10, // 10 resistori 330 ohm
         3L, 10, // 10 Pulsanti
         4L, 10, // 10 Led
+        5L, 10  // 10 Arduino Uno
+    );
+
+    private final Map<Long, Integer> DISP2 = Map.of(
+        // disponibilità nei miei inventari
+        // Ho in quantità abbondante quasi tutti gli elementi di BOM1
+        // Ma di Pulsanti ne ho solo 1 quando ne sono richiesti 2
+        1L, 10, // 10 resistori 100k ohm
+        2L, 10, // 10 resistori 330 ohm
+        3L, 1,  // 10 Pulsanti
+        4L, 10, // 10 Led
+        5L, 10  // 10 Arduino Uno
+    );
+
+    private final Map<Long, Integer> DISP3 = Map.of(
+        // disponibilità nei miei inventari
+        // Ho in quantità abbondante quasi tutti gli elementi di BOM1
+        // Ma non possiedo i Led
+        1L, 10, // 10 resistori 100k ohm
+        2L, 10, // 10 resistori 330 ohm
+        3L, 10, // 10 Pulsanti
         5L, 10  // 10 Arduino Uno
     );
 
