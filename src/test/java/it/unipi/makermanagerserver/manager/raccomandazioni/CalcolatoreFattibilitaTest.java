db56602a027a424ec98810f0369805a9782891d0
giuse27 <giuseppe.vaglica3@gmail.com>
2026-07-08 17:08:11 +0200

    ulteriori unit test

--- src/test/java/it/unipi/makermanagerserver/manager/raccomandazioni/CalcolatoreFattibilitaTest.java
+++ src/test/java/it/unipi/makermanagerserver/manager/raccomandazioni/CalcolatoreFattibilitaTest.java
@@ -84,6 +84,70 @@ public class CalcolatoreFattibilitaTest {
         
     }
 
+    /**
+     * TEST 4 con BOM2 e DISP4
+     * 
+     * Ho alcuni elementi duplicati nella bom e voglio 
+     * vedere se la quantità totale è corretta, per farlo testo su un progetto 
+     * di cui mi mancano alcuni pezzi
+     */
+    @Test
+    void testAggregazioneElementiRipetuti() {
+        
+        EsitoFattibilita esito = calcolatore.calcola(BOM2, DISP4);
+
+        assertFalse(esito.realizzabile());
+        assertEquals(2, esito.righeMancanti());
+        assertEquals(5, esito.righeTotali());
+        assertEquals(0.6, esito.indiceFattibilita());
+        // testo implicitamente anche l'ordine di comparizione
+        assertEquals(30, esito.dettaglioMancanti().getFirst().quantitaRichiesta());
+        assertEquals(27, esito.dettaglioMancanti().getFirst().quantitaPosseduta());
+        assertEquals(3, esito.dettaglioMancanti().getFirst().quantitaMancante());
+        assertEquals(30, esito.dettaglioMancanti().getLast().quantitaRichiesta());
+        assertEquals(13, esito.dettaglioMancanti().getLast().quantitaPosseduta());
+        assertEquals(17, esito.dettaglioMancanti().getLast().quantitaMancante());
+        assertEquals(20, esito.pezziMancantiTotali());
+
+    }
+
+    /**
+     * TEST 5 con BOM2 e inventario vuoto
+     * 
+     * Mi aspetto che sia tutto mancante, il test valuta anche la correttezza
+     * di pezziMancantiTotali()
+     */
+    @Test
+    void testInventarioVuotoRendeTuttoMancante() {
+ 
+        EsitoFattibilita esito = calcolatore.calcola(BOM2, Map.of());
+ 
+        assertFalse(esito.realizzabile());
+        assertEquals(5, esito.righeMancanti());
+        assertEquals(0.0, esito.indiceFattibilita());
+        assertEquals(63, esito.pezziMancantiTotali());
+
+    }
+
+    /**
+     * TEST 6 con BOM vuota e inventario vuoto
+     * 
+     * Mi aspetto che sia indice = 1.0, effettuo questo test per vedere se ci
+     * sono punti sensibili alla divisione per 0. Questo test nella pratica non
+     * compare mai in quanto i progetti con bom vuota vengono scartati
+     */
+    @Test
+    void testInventarioEBomVuota() {
+ 
+        EsitoFattibilita esito = calcolatore.calcola(List.of(), Map.of());
+ 
+        assertTrue(esito.realizzabile());
+        assertEquals(0, esito.righeMancanti());
+        assertEquals(1.0, esito.indiceFattibilita());
+        assertEquals(0, esito.pezziMancantiTotali());
+
+    }
+
     // UTILITY
 
     /**
@@ -151,6 +215,44 @@ public class CalcolatoreFattibilitaTest {
         1L, 10, // 10 resistori 100k ohm
         2L, 10, // 10 resistori 330 ohm
         3L, 10, // 10 Pulsanti
+        5L, 10  // 10 Arduino Uno
+    );
+
+    private final ElementoCatalogo elementoRipetuto1 = creaElemento(1L, "Resistore 100k ohm");
+    
+    private final List<RigaBOM> BOM2 = List.of(
+        new RigaBOM(
+            elementoRipetuto1,
+            10
+        ),
+        new RigaBOM(
+            creaElemento(2L, "Resistore 330 ohm"), 
+            1
+        ),
+        new RigaBOM(
+            creaElemento(3L, "Pulsanti"), 
+            30
+        ),
+        new RigaBOM(
+            creaElemento(4L, "Led"), 
+            1
+        ),
+        new RigaBOM(
+            creaElemento(5L, "Arduino Uno"), 
+            1
+        ),
+        new RigaBOM(
+            elementoRipetuto1,
+            20
+        )
+    );
+
+    private final Map<Long, Integer> DISP4 = Map.of(
+        // disponibilità nei miei inventari
+        1L, 27, // 10 resistori 100k ohm
+        2L, 10, // 10 resistori 330 ohm
+        3L, 13, // 10 Pulsanti
+        4L, 10, // 10 Led
         5L, 10  // 10 Arduino Uno
     );
 
